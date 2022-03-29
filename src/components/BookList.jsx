ae7606e42fc46b14162d57fb1fe7d3d63af90f21
miftah1991 <miftah.amin1991@gmail.com>
2022-03-30 00:40:25 +0430

    Remove Test Data

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,32 +3,13 @@ import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
 const BookList = () => {
-  const DUMMYDATA = [
-    {
-      id: '1',
-      title: 'Flutter Tuotrials',
-      author: 'Flutter Author',
-      category: 'Computer Science',
-    },
-    {
-      id: '2',
-      title: 'Micro Finance',
-      author: 'Robert',
-      category: 'Economics',
-    },
-    {
-      id: '3',
-      title: 'Computer Fundamentals',
-      author: 'Hassan',
-      category: 'Computer',
-    },
-  ];
+  
   const books = useSelector((state) => state.booksReducer);
 
   return (
     <div>
       <ul>
-      {[...DUMMYDATA, ...books].map((book) => (
+      {books.map((book) => (
           <BookItem
             key={book.id}
             id={book.id}

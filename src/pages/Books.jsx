6e1f377d045d54a57cd3c0d0db48d9010a9027a3
miftah1991 <miftah.amin1991@gmail.com>
2022-04-04 21:53:33 +0430

    Fix function declaration

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import BookList from '../components/BookList';
 import AddBook from '../components/AddBook';
 
-const Books = () => {
+const Books = () => (
   <>
     <BookList />
     <AddBook />
-  </>;
-};
+  </>
+);
 
 export default Books;

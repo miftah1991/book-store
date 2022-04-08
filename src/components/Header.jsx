6dd111c2f34e7b826de24dae7cdbedcb058147cc
miftah1991 <miftah.amin1991@gmail.com>
2022-04-08 22:08:55 +0430

    Required Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,24 @@ const Header = () => (
   <header className="container-fluid">
     <nav className="navbar navbar-expand-lg navbar-light bg-white p-5">
       <div className="container">
-        <h1 className="display-4">BookStore</h1>
+        <h1 className="display-4">BookStore CMS</h1>
 
         <div className="navbar-collapse">
           <ul className="navbar-nav m-3">
             <li className="nav-item active">
               <Link to="/books" className="nav-link">
-                Books
+                BOOKS
               </Link>
             </li>
             <li className="nav-item">
               <Link to="/categories" className="nav-link">
-                Categories
+                CATEGORIES
               </Link>
             </li>
           </ul>
+        </div>
+        <div>
+          <i className="fa fa-user"> </i>
         </div>
       </div>
     </nav>
